f75dc84a3fb882ae330ef72674f1a08a8b160fe4
lucaspaiva-lp <gml248@tutamail.com>
2025-11-15 16:06:51 -0300

    Revert "Update inimigo.py"
    This reverts commit 322acee9902868b6911bb15b09b8a75e3d54d689.

--- models/inimigo.py
+++ models/inimigo.py
@@ -3,59 +3,49 @@ from .base import Entidade, Atributos
 
 
 class Inimigo(Entidade):
-    def __init__(self, nome: str, vida: int, ataque: int, defesa: int):
-        super().__init__(nome, Atributos(vida=vida, ataque=ataque, defesa=defesa, vida_max=vida))
+    class inimigo:
+    def_init_(self,nome, vida, ataque, defesa, efeitos):
+        self.nome = nome
+        self.vida = vida
+        self.ataque = ataque
+        self.defesa = defesa
+        self.efeitos = {"sangramento": {"dano": 2, "turnos": 3}}
 
-    def get_vida(self):
-        return self._atrib.vida
 
-    def set_vida(self, nova_vida: int) -> None:
-            self._atrib.vida = nova_vida
-
-
-    def get_ataque(self):
-        return self._atrib.ataque
-    
-    def set_ataque(self,novo_ataque):
-        self._atrib.ataque = novo_ataque
-    
-
-    def get_defesa(self):
-        return self._atrib.defesa
-    
-    def set_defesa(self,nova_defesa):
-        self._atrib.defesa = nova_defesa
+    def calcular_dano_base(self):
+        return self.ataque
      
     def receber_dano(self, dano):
-        dano_final = max(0, dano - self._atrib.defesa)
-        self._atrib.vida -= dano_final
-
-        if self._atrib.vida <0:
-             self._atrib.vida = 0
+        dano_final = max(0, dano - self.defesa)
+        self.vida -= dano_final
+        if self.vida <0:
+        self.vida = 0
         return dano_final
                
     def esta_vivo(self):
-        return self._atrib.vida > 0
+        return self.vida > 0
      
 class goblin(Inimigo):
-    def __init__(self, nome):
-        super().__init__(nome, vida = 90, ataque = 5, defesa = 7)
-    def furia(self):
-        self._atrib.defesa()
-        self.set_defesa(nova_defesa = - 2)
-        self._atrib.ataque()
-        self.set_ataque(novo_ataque = + 2)
-
+    def_init_(self, nome):
+        super()._init_(nome, vida = 90, ataque = 7, defesa = 7)
+        
+    def habilidade_especial(self, alvo):
     #Furia goblin: o goblin se enfurece reduzindo sua defesa em -2 , e aumentando seu ataque em +2.
- 
+    if  self.defesa = 7:
+        self.defesa -= 2
+        self.ataque += 2
+        
+    def alvo.receber_dano(ataque)
+    return self.ataque
 
 class ladrao(Inimigo):
-    def __init__(self, nome):
-        super().__init__(nome, vida = 110, ataque = 11, defesa = 3)
-   
+    def_init_(self, nome):
+        super()._init_(nome, vida = 110, ataque = 11, defesa = 3)
+    def_habilidade_especial(self, alvo):
     # Disparo de Adaga: o ladrao arremessa uma de suas adagas
     # acertando o seu alvo e causando sangramento de -2 de hp por turno
-   
+    dano_base
 
-    
+    def __init__(self, nome: str, vida: int, ataque: int, defesa: int):
+        super().__init__(nome, Atributos(vida=vida, ataque=ataque, defesa=defesa, vida_max=vida))
 

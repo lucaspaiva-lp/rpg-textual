cfb9d00cda6f4148faee4f02179e44567866d2dc
lucaspaiva-lp <gml248@tutamail.com>
2025-11-18 15:02:39 -0300

    feat: atualização para versão modular

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ from models.jogo import Jogo
 
 
 def menu() -> None:
-    """Exibe o menu principal do jogo e gerencia a navegação entre opções.
+    """Exibe o menu principal do jogo e gerencia a navegação.
 
-    Cria uma instância da classe `Jogo` e permite que o jogador interaja
-    com as principais funcionalidades do sistema, como criação de personagem,
-    seleção de missões e simulação de salvamento/carregamento.
+    Cria uma instância da classe `Jogo` e permite ao jogador acessar:
+    - criação de personagem,
+    - missões,
+    - salvamento,
+    - carregamento.
 
-    Loop contínuo até que o usuário opte por encerrar o programa.
+    Loop contínuo até que o usuário escolha sair.
     """
     jogo = Jogo()
 
     while True:
         print("\n=== RPG OO — Menu Principal ===")
-        print("[1] Menu do personagem")
-        print("[2] Encarar missão")
+        print("[1] Criar personagem")
+        print("[2] Missão")
         print("[3] Salvar")
         print("[4] Carregar")
         print("[0] Sair")
@@ -24,7 +26,7 @@ def menu() -> None:
         op = input("> ").strip()
 
         if op == "1":
-            jogo.menu_personagem()
+            jogo.criar_personagem()
         elif op == "2":
             jogo.menu_missao()
         elif op == "3":
@@ -41,7 +43,6 @@ def menu() -> None:
 if __name__ == "__main__":
     """Ponto de entrada principal do programa.
 
-    Executa a função `menu()` caso o script seja chamado diretamente,
-    inicializando o fluxo principal do jogo.
+    Executa `menu()` caso o arquivo seja executado diretamente.
     """
     menu()
